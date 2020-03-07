0fd24f64db818ac1acc659d36fcf4a0fcb473de3
ClackHack <48455071+ClackHack@users.noreply.github.com>
2020-03-06 22:11:13 -0500

    Initial commit

---
+++ SocketAsyncTerminal.py
@@ -0,0 +1,66 @@
+import ui
+import socket as so
+import sys
+import console
+labeltext=''
+import socket
+sb = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sb.connect(("8.8.8.8", 80))
+ip = str(sb.getsockname()[0])
+sb.close()
+#target=input("IP: ")
+target="10.0.1.76"
+port=5001
+print("Connecting...")
+s=so.socket(so.AF_INET,so.SOCK_STREAM)
+s.connect((target,port))
+s.settimeout(1)
+def recv():
+	global s
+	global exit
+	while 1:
+		try:
+			if exit:
+				sys.exit()
+			data = s.recv(3000)
+			return str(data.decode('utf-8'))
+		except OSError:
+			pass
+def send():
+	global ip
+	global s
+	global labeltext
+	while 1:
+		text = input()
+		console.clear()
+		import time
+		if text == '':
+			global exit
+			exit=True
+			exitt()
+		print(labeltext)
+		if exit:
+			return
+			sys.exit()
+		s.send(bytes(str(text) + " " ,"utf-8"))
+def exitt():
+	print('Exiting...')
+	global s
+	s.close()
+	global exit
+	exit = True
+	sys.exit()
+#s.close()
+labeltext='Connecting...\nConnected'
+import _thread
+_thread.start_new_thread(send,())
+exit = False
+print('Connected')
+while 1:
+	try:
+		data = recv()
+		print(data)
+		labeltext = labeltext + str('\n'+data)
+	except TypeError as e:
+		pass
+
